7e10d2c836bb3d4998baf5fec81cdd0614e1deb2
suzhangjiegh <su2010yx@163.com>
2017-11-20 16:29:24 +0800

    store使用

--- src/components/HeadTop.vue
+++ src/components/HeadTop.vue
@@ -5,20 +5,43 @@
 			<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
 			<el-breadcrumb-item v-for="(item, index) in $route.meta" key="index">{{item}}</el-breadcrumb-item>
 		</el-breadcrumb>
-	<!--	<el-dropdown @command="handleCommand" menu-align='start'>
-			<img :src="baseImgPath + adminInfo.avatar" class="avator">
+
+		<el-dropdown  menu-align='start'>
+			<img :src="adminInfo.avatar" class="avator">
+			<label>{{adminInfo.name}}</label>
 			<el-dropdown-menu slot="dropdown">
-				<el-dropdown-item command="home">首页</el-dropdown-item>
-				<el-dropdown-item command="singout">退出</el-dropdown-item>
+				<el-dropdown-item >首页</el-dropdown-item>
+				<el-dropdown-item >退出</el-dropdown-item>
 			</el-dropdown-menu>
-		</el-dropdown>-->
+		</el-dropdown>
 	</div>
 </template>
 
 <script>
 
+	import {mapActions, mapState} from 'vuex'
+
 	export default {
 		name:'HeadTop',
+		created(){
+			if (!this.adminInfo.id) {
+				this.getAdminData()
+			}
+		},
+		computed: {
+			/**
+			 * 将store.state.adminInfo 映射this.adminInfo
+			 * 这个this 很重要，这个映射直接映射到当前Vue的this对象上。
+			 */
+			...mapState(['adminInfo']),
+		},
+		methods:{
+			/**
+			 * 将 `this.getAdminData()` 映射为 `this.$store.dispatch('getAdminData')`
+			 */
+			...mapActions(['getAdminData']),
+		},
+
 		beforeUpdate(){
 			console.log(this.$route.meta);
 		}
--- src/store/index.js
+++ src/store/index.js
@@ -11,19 +11,44 @@ Vue.use(Vuex)
  * 3
  *消息分发后 最终设置到state中
  */
-const state = {};
+const state = {
+	adminInfo:{}
+};
 
 /**
  * 2
  * 唯一标识符 store通过他用于异步与同步间通讯
  */
-const mutations = {};
+const mutations = {
+	saveAdminInfo(state, adminInfo){
+		state.adminInfo = adminInfo;
+	}
+};
 
 /**
  * 1
  * @type {{}}
  */
-const actions = {};
+const actions = {
+
+	// 应该异步 使用同步设置
+	/**
+	 * assets的加载使用require 或者 import
+	 * static的加载为路径加载
+	 * 方法二
+	 * import avatar from '../assets/avatar.png'
+	 * avatar:avatar,
+	 * @param commit
+	 */
+	getAdminData({commit}){
+		commit('saveAdminInfo', {
+			name:'小小',
+			avatar:'./static/avatar.png',
+			id:123
+		});
+	}
+
+};
 
 export default new Vuex.Store({
 	state,
